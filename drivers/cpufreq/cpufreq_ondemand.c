1c126856b3af773894e0b72217a42192252bd573
hyesung.shin <hyesung.shin@lge.com>
2010-08-09 13:27:04 +0900

    [Common] QTC patch for power consumption 	- Not bumping up CPU freq for sensor event
    Signed-off-by: cleaneye <cleaneye.kim@lge.com>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -703,7 +703,13 @@ static DECLARE_WORK(dbs_refresh_work, dbs_refresh_callback);
 static void dbs_input_event(struct input_handle *handle, unsigned int type,
 		unsigned int code, int value)
 {
-	schedule_work(&dbs_refresh_work);
+	if(!strcmp((char*)(handle->dev->name), "accelerometer") || !strcmp((char*)(handle->dev->name), "Acompass")){
+		//printk(KERN_INFO "Not Bumping up CPU for %s", handle->dev->name);
+		return;
+	}else{
+		schedule_work(&dbs_refresh_work);
+	}
+
 }
 
 static int dbs_input_connect(struct input_handler *handler,
