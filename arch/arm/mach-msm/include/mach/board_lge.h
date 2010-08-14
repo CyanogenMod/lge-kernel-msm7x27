604af7cd0d25d570327eecf68ddbeee1bdd0a5c3
varun colbert <varuncolbert@gmail.com>
2010-08-14 02:47:51 -0700

    Merge remote branch 'remotes/CAF/froyo_almond' into 7x27_common-2.6.32
    - Also merge the QCOM board file to LG board file
      change to increase PMEM ADSP size (commit ID 2d34ebfcc5d0a04de149f7d402da6b49ee27176a)
    
    Conflicts:
    	drivers/char/diag/diagchar_core.c
    
    Signed-off-by: cleaneye <cleaneye.kim@lge.com>

--- arch/arm/mach-msm/include/mach/board_lge.h
+++ arch/arm/mach-msm/include/mach/board_lge.h
@@ -35,15 +35,17 @@
 #endif
 
 /* define PMEM address size */
-#define MSM_PMEM_MDP_SIZE      0x1700000
-#define MSM_PMEM_ADSP_SIZE     0xAE4000
-#define MSM_PMEM_AUDIO_SIZE    0x5B000
-#define MSM_FB_SIZE            0x177000
-#define MSM_GPU_PHYS_SIZE      SZ_2M
-#define PMEM_KERNEL_EBI1_SIZE  0x1C000
+#ifdef CONFIG_ARCH_MSM7X27
+#define MSM_PMEM_MDP_SIZE	0x1B76000
+#define MSM_PMEM_ADSP_SIZE	0xB71000
+#define MSM_PMEM_AUDIO_SIZE	0x5B000
+#define MSM_FB_SIZE		0x177000
+#define MSM_GPU_PHYS_SIZE	SZ_2M
+#define PMEM_KERNEL_EBI1_SIZE	0x1C000
 
 /* Using lower 1MB of OEMSBL memory for GPU_PHYS */
 #define MSM_GPU_PHYS_START_ADDR	 0xD600000ul
+#endif
 
 /* Using upper 1/2MB of Apps Bootloader memory*/
 #define MSM_PMEM_AUDIO_START_ADDR	0x80000ul
