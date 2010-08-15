db43322a0753f76c0057972313e9cb00f3cda2da
Park Hyun Hui <hyunhui.park@lge.com>
2010-08-15 18:00:24 +0900

    [VS660][USB] Adjust init pid at init process.
    As side effect of pid initialization in VS660. VS660 initialize
    the pid of USB by init process.
    
    Signed-off-by: cleaneye <cleaneye.kim@lge.com>

--- arch/arm/mach-msm/lge/board-thunderc.c
+++ arch/arm/mach-msm/lge/board-thunderc.c
@@ -140,6 +140,8 @@ struct android_usb_platform_data android_usb_pdata = {
 	.product_name       = "LG Android USB Device",
 	.manufacturer_name	= "LG Electronics Inc.",
 	.serial_number		= "LGANDROIDVS660",	
+	/* Thunderc is not below pid and init process is charge of 
+	   initialization of pid */
 	.init_product_id	= 0x618E,
 	.nluns = 1,
 };
