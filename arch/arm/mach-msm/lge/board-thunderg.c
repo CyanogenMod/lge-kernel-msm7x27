49beb62aa46f85f3ccdc645b18ac56eba8b4a0de
varun colbert <varuncolbert@gmail.com>
2010-08-27 13:51:08 -0700

    Merge board 7x27 change to lge board change for wlan
    (merge commit fdac1188ceb4defb7a9df313a9acbef702ce867b
    wireless: WCN1312: init wlan deep sleep signal to low instead of floating)
    
    Signed-off-by: cleaneye <cleaneye.kim@lge.com>

--- arch/arm/mach-msm/lge/board-thunderg.c
+++ arch/arm/mach-msm/lge/board-thunderg.c
@@ -214,6 +214,19 @@ static struct msm_acpu_clock_platform_data msm7x2x_clock_data = {
 void msm_serial_debug_init(unsigned int base, int irq,
 			   struct device *clk_device, int signal_irq);
 
+static void msm7x27_wlan_init(void)
+{
+	int rc = 0;
+	/* TBD: if (machine_is_msm7x27_ffa_with_wcn1312()) */
+	if (machine_is_msm7x27_ffa()) {
+		rc = mpp_config_digital_out(3, MPP_CFG(MPP_DLOGIC_LVL_MSMP,
+				MPP_DLOGIC_OUT_CTRL_LOW));
+		if (rc)
+			printk(KERN_ERR "%s: return val: %d \n",
+				__func__, rc);
+	}
+}
+
 /* decrease FB pmem size because thunderg uses hvga
  * qualcomm's original value depends on wvga resolution
  * 2010-04-18, cleaneye.kim@lge.com
@@ -262,6 +275,7 @@ static void __init msm7x2x_init(void)
 	else
 		msm_pm_set_platform_data(msm7x25_pm_data,
 					ARRAY_SIZE(msm7x25_pm_data));
+	msm7x27_wlan_init();
 
 #ifdef CONFIG_ANDROID_RAM_CONSOLE
 	lge_add_ramconsole_devices();
