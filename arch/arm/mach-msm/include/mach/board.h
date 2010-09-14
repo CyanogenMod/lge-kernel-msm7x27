4e9720642e61638af93f09fcee3c79d309d51151
Philip Elcan <pelcan@codeaurora.org>
2010-09-14 17:26:15 -0400

    msm: Add platform support for VGA on 8x60 FFA
    Add platform support to toggle between the on board LCDC display
    panel and the external VGA out.
    
    Change-Id: Id94e0a9d439cdc7fe37eaf8e9bb7f1197481ce93
    Signed-off-by: Philip Elcan <pelcan@codeaurora.org>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -210,6 +210,7 @@ struct msm_panel_common_pdata {
 	int (*pmic_backlight)(int level);
 	int (*panel_num)(void);
 	void (*panel_config_gpio)(int);
+	int (*vga_switch)(int select_vga);
 	int *gpio_num;
 	int mdp_core_clk_rate;
 };
