5bb88c86524d5419c85ddd77fdc0a474655ecc03
Gregory Bean <gbean@codeaurora.org>
2010-09-15 09:54:43 -0700

    msm: synchronize gpiomux with upstream.
    Now that gpiomux is in the upstream kernel, merge all the changes which
    had to be made to secure acceptance into msm-2.6.35 in order to bring
    the two branches into agreement.
    
    Change-Id: I4e8c9f0c0bebfbbbcde8235397dc6d5adb9e40b8
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>

--- arch/arm/mach-msm/gpiomux.h
+++ arch/arm/mach-msm/gpiomux.h
@@ -1,40 +1,29 @@
 /* Copyright (c) 2010, Code Aurora Forum. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *     * Neither the name of Code Aurora Forum, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
  *
- * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  */
 #ifndef __ARCH_ARM_MACH_MSM_GPIOMUX_H
 #define __ARCH_ARM_MACH_MSM_GPIOMUX_H
 
 #include <linux/bitops.h>
+#include <linux/errno.h>
 
 #if defined(CONFIG_MSM_V2_TLMM)
-#include "gpiomux-8x60.h"
+#include "gpiomux-v2.h"
 #else
-#include "gpiomux-7x00.h"
+#include "gpiomux-v1.h"
 #endif
 
 /**
@@ -72,6 +61,8 @@ enum {
 	GPIOMUX_CTL_MASK = GPIOMUX_VALID,
 };
 
+#ifdef CONFIG_MSM_GPIOMUX
+
 /* Each architecture must provide its own instance of this table.
  * To avoid having gpiomux manage any given gpio, one or both of
  * the entries can avoid setting GPIOMUX_VALID - the absence
@@ -102,5 +93,22 @@ int msm_gpiomux_write(unsigned gpio,
  * should use msm_gpiomux_write.
  */
 void __msm_gpiomux_write(unsigned gpio, gpiomux_config_t val);
+#else
+static inline int __must_check msm_gpiomux_get(unsigned gpio)
+{
+	return -ENOSYS;
+}
 
+static inline int msm_gpiomux_put(unsigned gpio)
+{
+	return -ENOSYS;
+}
+
+static inline int msm_gpiomux_write(unsigned gpio,
+				    gpiomux_config_t active,
+				    gpiomux_config_t suspended)
+{
+	return -ENOSYS;
+}
+#endif
 #endif
