cbaa40978e249aa185425f3cdeb579075a7c1491
Niranjana Vishwanathapura <nvishwan@codeaurora.org>
2010-09-28 16:51:36 -0600

    msm: rmnet_sdio: change sdio net device names to rmnet_sdioX
    Change the sdio rmnet net device names so that it won't
    conflict with the smd rmnet net device namespace.
    
    Change-Id: I44c86f3ce83879df50e7751ae6d85f1ff8086952
    Signed-off-by: Niranjana Vishwanathapura <nvishwan@codeaurora.org>

--- drivers/net/msm_rmnet_common.c
+++ drivers/net/msm_rmnet_common.c
@@ -568,9 +568,9 @@ static int __init rmnet_init(void)
 #endif
 #endif
 
-	for (n = 3; n < 11; n++) {
+	for (n = 0; n < 8; n++) {
 		dev = alloc_netdev(sizeof(struct rmnet_private),
-				   "rmnet%d", rmnet_setup);
+				   "rmnet_sdio%d", rmnet_setup);
 
 		if (!dev)
 			return -ENOMEM;
@@ -579,7 +579,7 @@ static int __init rmnet_init(void)
 		p = netdev_priv(dev);
 		/* Initial config uses Ethernet */
 		p->operation_mode = RMNET_MODE_LLP_ETH;
-		p->ch_id = n - 3;
+		p->ch_id = n;
 		spin_lock_init(&p->lock);
 #ifdef CONFIG_MSM_RMNET_DEBUG
 		p->timeout_us = timeout_us;
