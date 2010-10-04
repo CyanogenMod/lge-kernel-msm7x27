b4094391e5b2971c2ee8b7a5b3bf6133095a02d3
Sushmita Susheelendra <ssusheel@codeaurora.org>
2010-10-04 10:21:20 -0600

    AndroidKernel.mk: Install sanitized linux kernel headers
    Invoking the kernel headers_install target to install sanitized
    kernel headers. Sanitized kernel headers will be installed in
    out/target/product/<TARGET_PRODUCT>/obj/KERNEL_OBJ/usr.
    Userspace applications are supposed to include linux kernel
    headers from this location and not from under the kernel
    source tree directly.
    
    Change-Id: Ia6527fc83673d162834cc5cd416f799af84540fb
    Signed-off-by: Sushmita Susheelendra <ssusheel@codeaurora.org>

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -5,6 +5,8 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
+KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
+
 ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),true)
 $(info Using uncompressed kernel)
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/piggy
@@ -21,9 +23,12 @@ $(KERNEL_CONFIG): $(KERNEL_OUT)
 $(KERNEL_OUT)/piggy : $(TARGET_PREBUILT_INT_KERNEL)
 	$(hide) gunzip -c $(KERNEL_OUT)/arch/arm/boot/compressed/piggy.gzip > $(KERNEL_OUT)/piggy
 
-$(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
+$(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_INSTALL)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi-
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules
+
+$(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT) $(KERNEL_CONFIG)
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- headers_install
 
 kerneltags: $(KERNEL_OUT) $(KERNEL_CONFIG)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- tags
