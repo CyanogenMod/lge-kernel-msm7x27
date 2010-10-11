957bbadab433ec4a4f24830dcdd38bd30394eaf4
Ravishangar Kalyanam <rkalya@codeaurora.org>
2010-10-11 12:09:21 -0700

    msm_fb: display: Add MDDI multiple data read/write support
    Change-Id: If34dcfb0ffca650f00d5cc897fc79ff42ad939bc
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    CRs-fixed: 258225

--- drivers/video/msm/mddihost.c
+++ drivers/video/msm/mddihost.c
@@ -235,15 +235,15 @@ int mddi_host_register_write(uint32 reg_addr,
 	regacc_pkt_ptr->bClient_ID = 0;
 	regacc_pkt_ptr->read_write_info = 0x0001;
 	regacc_pkt_ptr->register_address = reg_addr;
-	regacc_pkt_ptr->register_data_list = reg_val;
+	regacc_pkt_ptr->register_data_list[0] = reg_val;
 
 	MDDI_MSG_DEBUG("Reg Access write reg=0x%x, value=0x%x\n",
 		       regacc_pkt_ptr->register_address,
-		       regacc_pkt_ptr->register_data_list);
+		       regacc_pkt_ptr->register_data_list[0]);
 
 	regacc_pkt_ptr = &curr_llist_dma_ptr->packet_header.register_pkt;
 	curr_llist_ptr->packet_data_pointer =
-	    (void *)(&regacc_pkt_ptr->register_data_list);
+	    (void *)(&regacc_pkt_ptr->register_data_list[0]);
 
 	/* now adjust pointers */
 	mddi_queue_forward_packets(curr_llist_idx, curr_llist_idx, wait,
@@ -366,11 +366,11 @@ boolean mddi_host_register_write_int
 	regacc_pkt_ptr->bClient_ID = 0;
 	regacc_pkt_ptr->read_write_info = 0x0001;
 	regacc_pkt_ptr->register_address = reg_addr;
-	regacc_pkt_ptr->register_data_list = reg_val;
+	regacc_pkt_ptr->register_data_list[0] = reg_val;
 
 	regacc_pkt_ptr = &curr_llist_dma_ptr->packet_header.register_pkt;
 	curr_llist_ptr->packet_data_pointer =
-	    (void *)(&(regacc_pkt_ptr->register_data_list));
+	    (void *)(&(regacc_pkt_ptr->register_data_list[0]));
 
 	/* now adjust pointers */
 	mddi_queue_forward_packets(curr_llist_idx, curr_llist_idx, FALSE,
@@ -444,5 +444,171 @@ uint16 mddi_assign_pkt_height(uint16 pkt_width,
 		new_pkt_height = pkt_height;
 
 	return new_pkt_height;
+}
+#endif
+
+#ifdef ENABLE_MDDI_MULTI_READ_WRITE
+int mddi_host_register_multiwrite(uint32 reg_addr,
+	uint32 *value_list_ptr,
+	uint32 value_count, boolean wait, mddi_llist_done_cb_type done_cb,
+	mddi_host_type host)
+{
+	mddi_linked_list_type *curr_llist_ptr;
+	mddi_linked_list_type *curr_llist_dma_ptr;
+	mddi_register_access_packet_type *regacc_pkt_ptr;
+	uint16 curr_llist_idx;
+	int ret = 0;
+
+	if (!value_list_ptr || !value_count ||
+		value_count > MDDI_HOST_MAX_CLIENT_REG_IN_SAME_ADDR) {
+		MDDI_MSG_ERR("\n Invalid value_list or value_count");
+		return -EINVAL;
+	}
+
+	if (in_interrupt())
+		MDDI_MSG_CRIT("Called from ISR context\n");
+
+	if (!mddi_host_powered) {
+		MDDI_MSG_ERR("MDDI powered down!\n");
+		mddi_init();
+	}
+
+	down(&mddi_host_mutex);
+
+	curr_llist_idx = mddi_get_next_free_llist_item(host, TRUE);
+	curr_llist_ptr = &llist_extern[host][curr_llist_idx];
+	curr_llist_dma_ptr = &llist_dma_extern[host][curr_llist_idx];
+
+	curr_llist_ptr->link_controller_flags = 1;
+	curr_llist_ptr->packet_header_count = 14;
+	curr_llist_ptr->packet_data_count =
+		(uint16)(value_count * 4);
+
+	curr_llist_ptr->next_packet_pointer = NULL;
+	curr_llist_ptr->reserved = 0;
+
+	regacc_pkt_ptr = &curr_llist_ptr->packet_header.register_pkt;
+
+	regacc_pkt_ptr->packet_length = curr_llist_ptr->packet_header_count
+		+ curr_llist_ptr->packet_data_count;
+	regacc_pkt_ptr->packet_type = 146;	/* register access packet */
+	regacc_pkt_ptr->bClient_ID = 0;
+	regacc_pkt_ptr->read_write_info = value_count;
+	regacc_pkt_ptr->register_address = reg_addr;
+	memcpy((void *)&regacc_pkt_ptr->register_data_list[0], value_list_ptr,
+		   curr_llist_ptr->packet_data_count);
+
+	curr_llist_ptr->packet_data_pointer =
+		(void *)(&regacc_pkt_ptr->register_data_list[0]);
+	MDDI_MSG_DEBUG("Reg Access write reg=0x%x, value=0x%x\n",
+		       regacc_pkt_ptr->register_address,
+		       regacc_pkt_ptr->register_data_list[0]);
+
+	/* now adjust pointers */
+	mddi_queue_forward_packets(curr_llist_idx, curr_llist_idx, wait,
+				   done_cb, host);
+
+	up(&mddi_host_mutex);
+
+	if (wait) {
+		int wait_ret;
+
+		mddi_linked_list_notify_type *llist_notify_ptr;
+		llist_notify_ptr = &llist_extern_notify[host][curr_llist_idx];
+		wait_ret = wait_for_completion_timeout(
+					&(llist_notify_ptr->done_comp), 5 * HZ);
+
+		if (wait_ret <= 0)
+			ret = -EBUSY;
+
+		if (wait_ret < 0)
+			printk(KERN_ERR "%s: failed to wait for completion!\n",
+				__func__);
+		else if (!wait_ret)
+			printk(KERN_ERR "%s: Timed out waiting!\n", __func__);
+	}
+
+	return ret;
+}
+
+int mddi_host_register_multiread(uint32 reg_addr,
+	uint32 *value_list_ptr, uint32 value_count,
+	boolean wait, mddi_host_type host) {
+	mddi_linked_list_type *curr_llist_ptr;
+	mddi_register_access_packet_type *regacc_pkt_ptr;
+	uint16 curr_llist_idx;
+	int ret = 0;
+
+	if (!value_list_ptr || !value_count ||
+		value_count >= MDDI_HOST_MAX_CLIENT_REG_IN_SAME_ADDR) {
+		MDDI_MSG_ERR("\n Invalid value_list or value_count");
+		return -EINVAL;
+	}
+
+	if (in_interrupt())
+		MDDI_MSG_CRIT("Called from ISR context\n");
+
+	if (!mddi_host_powered) {
+		MDDI_MSG_ERR("MDDI powered down!\n");
+		mddi_init();
+	}
+
+	down(&mddi_host_mutex);
+
+	mddi_reg_read_value_ptr = value_list_ptr;
+	curr_llist_idx = mddi_get_reg_read_llist_item(host, TRUE);
+	if (curr_llist_idx == UNASSIGNED_INDEX) {
+		up(&mddi_host_mutex);
+
+		/* need to change this to some sort of wait */
+		MDDI_MSG_ERR("Attempting to queue up more than 1 reg read\n");
+		return -EINVAL;
+	}
+
+	curr_llist_ptr = &llist_extern[host][curr_llist_idx];
+	curr_llist_ptr->link_controller_flags = 0x11;
+	curr_llist_ptr->packet_header_count = 14;
+	curr_llist_ptr->packet_data_count = 0;
+
+	curr_llist_ptr->next_packet_pointer = NULL;
+	curr_llist_ptr->packet_data_pointer = NULL;
+	curr_llist_ptr->reserved = 0;
+
+	regacc_pkt_ptr = &curr_llist_ptr->packet_header.register_pkt;
+
+	regacc_pkt_ptr->packet_length = curr_llist_ptr->packet_header_count;
+	regacc_pkt_ptr->packet_type = 146;	/* register access packet */
+	regacc_pkt_ptr->bClient_ID = 0;
+	regacc_pkt_ptr->read_write_info = 0x8000 | value_count;
+	regacc_pkt_ptr->register_address = reg_addr;
+
+	/* now adjust pointers */
+	mddi_queue_forward_packets(curr_llist_idx, curr_llist_idx, wait,
+				   NULL, host);
+	/* need to check if we can write the pointer or not */
+
+	up(&mddi_host_mutex);
+
+	if (wait) {
+		int wait_ret;
+
+		mddi_linked_list_notify_type *llist_notify_ptr;
+		llist_notify_ptr = &llist_extern_notify[host][curr_llist_idx];
+		wait_ret = wait_for_completion_timeout(
+					&(llist_notify_ptr->done_comp), 5 * HZ);
+
+		if (wait_ret <= 0)
+			ret = -EBUSY;
+
+		if (wait_ret < 0)
+			printk(KERN_ERR "%s: failed to wait for completion!\n",
+				__func__);
+		else if (!wait_ret)
+			printk(KERN_ERR "%s: Timed out waiting!\n", __func__);
+	}
+
+	MDDI_MSG_DEBUG("Reg Read value=0x%x\n", *value_list_ptr);
+
+	return ret;
 }
 #endif
--- drivers/video/msm/msm_fb_def.h
+++ drivers/video/msm/msm_fb_def.h
@@ -214,4 +214,6 @@ extern unsigned char *msm_emdh_base;
 extern unsigned char *mipi_dsi_base;
 #endif
 
+#undef ENABLE_MDDI_MULTI_READ_WRITE
+
 #endif /* MSM_FB_DEF_H */
