302e36a9907723d9b48d37297072b64ac078cd76
Ravishangar Kalyanam <rkalya@codeaurora.org>
2010-10-12 19:59:42 -0700

    msm_fb: display: Add dynamic MIPI panel detection support
    Change-Id: Ie1281a353c15c7d7575eb1b591d325382d61c303
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>

--- drivers/video/msm/mipi_toshiba_video_wvga_pt.c
+++ drivers/video/msm/mipi_toshiba_video_wvga_pt.c
@@ -26,6 +26,11 @@ static int __init mipi_video_toshiba_wvga_pt_init(void)
 {
 	int ret;
 
+#ifdef CONFIG_FB_MSM_MIPI_PANEL_DETECT
+	if (msm_fb_detect_client("mipi_video_toshiba_wvga"))
+		return 0;
+#endif
+
 	pinfo.xres = 480;
 	pinfo.yres = 856;
 	pinfo.type = MIPI_VIDEO_PANEL;
