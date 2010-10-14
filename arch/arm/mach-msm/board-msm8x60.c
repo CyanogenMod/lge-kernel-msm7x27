4e0a4759775a95a168095a35d9c22d5086849060
Chetan Kalyan <ckalyan@codeaurora.org>
2010-10-14 13:28:15 -0700

    msm: board-8x60: Increase MSM_PMEM_SF_SIZE to 64MB
    SurfaceFlinger needs more PMEM to handle the 1024X600 screen in 8x60.
    
    Change-Id: I1b9bcb2ae79e05ebcdbcfeb7fd65fa7df5c4bab0
    Signed-off-by: Chetan Kalyan <ckalyan@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -1125,7 +1125,7 @@ static struct platform_device msm_batt_device = {
 #else /* CONFIG_FB_MSM_HDMI_MSM_PANEL */
 #define MSM_FB_SIZE 0x500000
 #endif /* CONFIG_FB_MSM_HDMI_MSM_PANEL */
-#define MSM_PMEM_SF_SIZE 0x1700000 /* 23 Mbytes */
+#define MSM_PMEM_SF_SIZE 0x4000000 /* 64 Mbytes */
 #define MSM_GPU_PHYS_SIZE 0x4cf000 /* 4.8 Mbytes */
 		/* 8* (512KB + 68384 + 32KB) + 36KB = 5040384 = 0x4ce900 */
 
