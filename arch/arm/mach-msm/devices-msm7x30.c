c4e8ba418b4d426a15cd5768bc8400395fac98bf
Stephen Boyd <sboyd@codeaurora.org>
2010-10-25 10:19:37 -0700

    msm: clock-7x30: Add support for CE_CLK
    The crypto driver needs support for this clock. Add it.
    
    Change-Id: Ib368cf3c8e54287b9bce593cc852972048ba87c0
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/devices-msm7x30.c
+++ arch/arm/mach-msm/devices-msm7x30.c
@@ -978,6 +978,7 @@ struct clk msm_clocks_7x30[] = {
 	CLK_7X30("adm_clk",	ADM_CLK,	NULL, 0),
 	CLK_7X30("cam_m_clk",	CAM_M_CLK,	NULL, 0),
 	CLK_7X30("camif_pad_pclk",	CAMIF_PAD_P_CLK,	NULL, OFF),
+	CLK_7X30("ce_clk",	CE_CLK,		NULL, 0),
 	CLK_7X30("emdh_clk",	EMDH_CLK,	NULL, OFF | CLK_MINMAX),
 	CLK_7X30("emdh_pclk",	EMDH_P_CLK,	NULL, OFF),
 	CLK_7X30("grp_2d_clk",	GRP_2D_CLK,	NULL, 0),
