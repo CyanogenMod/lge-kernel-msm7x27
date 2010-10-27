066fcbbcb81298a66af8d85cb06c90efb5422fac
Mohan Pallaka <mpallaka@codeaurora.org>
2010-10-27 13:02:17 +0530

    msm: 7x30: Add panel and display resolution for ttsp
    Add max coordiantes of x,y for panel and dispaly as
    platform data for ttsp touch screen driver.
    
    Change-Id: I903b5ca7eaf13da773387867393c998f67f16907
    Signed-off-by: Mohan Pallaka <mpallaka@codeaurora.org>

--- arch/arm/mach-msm/board-msm7x30.c
+++ arch/arm/mach-msm/board-msm7x30.c
@@ -341,6 +341,10 @@ static int cyttsp_platform_resume(struct i2c_client *client)
 }
 
 static struct cyttsp_platform_data cyttsp_data = {
+	.panel_maxx = 479,
+	.panel_maxy = 799,
+	.disp_maxx = 479,
+	.disp_maxy = 799,
 	.flags = 0,
 	.gen = CY_GEN3,	/* or */
 	.use_st = CY_USE_ST,
