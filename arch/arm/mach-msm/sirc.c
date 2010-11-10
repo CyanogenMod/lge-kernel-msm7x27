4672e5a7213ad0d6e1e39f3cc5011e08844fba80
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2010-11-10 10:35:04 -0800

    msm: sirc: fix copyright
    Change to Code Aurora Forum Copyright.
    
    Change-Id: I1d7f99b39fdd7f73010932f9aec121ec0acc66e8
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- arch/arm/mach-msm/sirc.c
+++ arch/arm/mach-msm/sirc.c
@@ -1,6 +1,6 @@
 /* linux/arch/arm/mach-msm/irq.c
  *
- * Copyright (c) 2009-2010 QUALCOMM Incorporated.
+ * Copyright (c) 2009-2010 Code Aurora Forum. All rights reserved.
  * Copyright (C) 2009 Google, Inc.
  *
  * This software is licensed under the terms of the GNU General Public
