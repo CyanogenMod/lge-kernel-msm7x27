18cc55a7bf604e5d2a1418fca24c4bdbef2d8e28
Pradeep Jilagam <pjilagam@codeaurora.org>
2010-11-13 12:48:43 +0530

    msm: qdsp5v2: audio: Enable Dual Mic End-fire devices for Timpani
    Add the platform devices and the corresponding profiles to
    support Dual Mic in End-fire configuration with Timpani
    
    Change-Id: Ib03fa4392659d42054c306168d3b42c2d81e50ac
    Signed-off-by: Pradeep Jilagam <pjilagam@codeaurora.org>

--- arch/arm/mach-msm/qdsp5v2/snddev_data_timpani.c
+++ arch/arm/mach-msm/qdsp5v2/snddev_data_timpani.c
@@ -318,6 +318,80 @@ static struct platform_device msm_ispkr_mic_device = {
 	.dev = { .platform_data = &snddev_ispkr_mic_data },
 };
 
+static struct adie_codec_action_unit idual_mic_endfire_8KHz_osr256_actions[] =
+	AMIC_DUAL_8000_OSR_256;
+
+static struct adie_codec_hwsetting_entry idual_mic_endfire_settings[] = {
+	{
+		.freq_plan = 8000,
+		.osr = 256,
+		.actions = idual_mic_endfire_8KHz_osr256_actions,
+		.action_sz = ARRAY_SIZE(idual_mic_endfire_8KHz_osr256_actions),
+	}, /* 8KHz profile can be used for 16KHz */
+	{
+		.freq_plan = 16000,
+		.osr = 256,
+		.actions = idual_mic_endfire_8KHz_osr256_actions,
+		.action_sz = ARRAY_SIZE(idual_mic_endfire_8KHz_osr256_actions),
+	}, /* 8KHz profile can be used for 48KHz */
+	{
+		.freq_plan = 48000,
+		.osr = 256,
+		.actions = idual_mic_endfire_8KHz_osr256_actions,
+		.action_sz = ARRAY_SIZE(idual_mic_endfire_8KHz_osr256_actions),
+	}
+};
+
+static struct adie_codec_dev_profile idual_mic_endfire_profile = {
+	.path_type = ADIE_CODEC_TX,
+	.settings = idual_mic_endfire_settings,
+	.setting_sz = ARRAY_SIZE(idual_mic_endfire_settings),
+};
+
+static enum hsed_controller idual_mic_endfire_pmctl_id[] = {
+	PM_HSED_CONTROLLER_0, PM_HSED_CONTROLLER_2
+};
+
+static struct snddev_icodec_data snddev_idual_mic_endfire_data = {
+	.capability = (SNDDEV_CAP_TX | SNDDEV_CAP_VOICE),
+	.name = "handset_dual_mic_endfire_tx",
+	.copp_id = 0,
+	.acdb_id = ACDB_ID_HANDSET_MIC_ENDFIRE,
+	.profile = &idual_mic_endfire_profile,
+	.channel_mode = 2,
+	.default_sample_rate = 48000,
+	.pmctl_id = idual_mic_endfire_pmctl_id,
+	.pmctl_id_sz = ARRAY_SIZE(idual_mic_endfire_pmctl_id),
+	.pamp_on = NULL,
+	.pamp_off = NULL,
+};
+
+static struct platform_device msm_idual_mic_endfire_device = {
+	.name = "snddev_icodec",
+	.id = 12,
+	.dev = { .platform_data = &snddev_idual_mic_endfire_data },
+};
+
+static struct snddev_icodec_data snddev_spk_idual_mic_endfire_data = {
+	.capability = (SNDDEV_CAP_TX | SNDDEV_CAP_VOICE),
+	.name = "speaker_dual_mic_endfire_tx",
+	.copp_id = 0,
+	.acdb_id = ACDB_ID_SPKR_PHONE_MIC_ENDFIRE,
+	.profile = &idual_mic_endfire_profile,
+	.channel_mode = 2,
+	.default_sample_rate = 48000,
+	.pmctl_id = idual_mic_endfire_pmctl_id,
+	.pmctl_id_sz = ARRAY_SIZE(idual_mic_endfire_pmctl_id),
+	.pamp_on = NULL,
+	.pamp_off = NULL,
+};
+
+static struct platform_device msm_spk_idual_mic_endfire_device = {
+	.name = "snddev_icodec",
+	.id = 14,
+	.dev = { .platform_data = &snddev_spk_idual_mic_endfire_data },
+};
+
 static struct platform_device *snd_devices_ffa[] __initdata = {
 	&msm_iearpiece_ffa_device,
 	&msm_imic_ffa_device,
@@ -328,6 +402,8 @@ static struct platform_device *snd_devices_ffa[] __initdata = {
 	&msm_bt_sco_mic_device,
 	&msm_ispkr_mic_device,
 	&msm_headset_stereo_device,
+	&msm_idual_mic_endfire_device,
+	&msm_spk_idual_mic_endfire_device,
 };
 
 void __ref msm_snddev_init_timpani(void)
