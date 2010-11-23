373843806be03badd4fe523e974eb83a35f4f8f8
Ricardo Cerqueira <github@cerqueira.org>
2010-11-23 15:20:46 +0000

    msm: Add CONFIG_MSM7X27_OVERCLOCK
    Optional support for adding acpuclock frequencies and voltages.
    
    Includes BACONMAKER for additional dangerous freqs. This
    should not to be enabled, ever.

--- arch/arm/mach-msm/acpuclock.c
+++ arch/arm/mach-msm/acpuclock.c
@@ -210,6 +210,16 @@ static struct clkctl_acpu_speed pll0_960_pll1_245_pll2_1200[] = {
 	{ 0, 400000, ACPU_PLL_2, 2, 2, 133333, 2, 5, 122880 },
 	{ 1, 480000, ACPU_PLL_0, 4, 1, 160000, 2, 6, 122880 },
 	{ 1, 600000, ACPU_PLL_2, 2, 1, 200000, 2, 7, 122880 },
+#ifdef CONFIG_MSM7X27_OVERCLOCK
+	{ 1, 768000, ACPU_PLL_0, 4, 1, 192000, 3, 7, 192000 },
+	{ 1, 787200, ACPU_PLL_0, 4, 1, 196800, 3, 7, 196800 },
+#ifdef CONFIG_MSM7X27_BACONMAKER
+	{ 1, 806400, ACPU_PLL_0, 4, 1, 201600, 3, 7, 201600 },
+	{ 1, 825600, ACPU_PLL_0, 4, 1, 206400, 3, 7, 206400 },
+	{ 1, 844800, ACPU_PLL_0, 4, 1, 211200, 3, 7, 211200 },
+	{ 1, 864000, ACPU_PLL_0, 4, 1, 216000, 3, 7, 216000 },
+#endif
+#endif
 	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {0, 0, 0}, {0, 0, 0} }
 };
 
@@ -424,11 +434,31 @@ static int acpuclk_set_vdd_level(int vdd)
 static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 {
 	uint32_t reg_clkctl, reg_clksel, clk_div, src_sel;
+#ifdef CONFIG_MSM7X27_OVERCLOCK
+	uint32_t a11_div;
+#endif
 
 	reg_clksel = readl(A11S_CLK_SEL_ADDR);
 
 	/* AHB_CLK_DIV */
 	clk_div = (reg_clksel >> 1) & 0x03;
+#ifdef CONFIG_MSM7X27_OVERCLOCK
+	a11_div = hunt_s->a11clk_src_div;
+
+	if (hunt_s->a11clk_khz > 600000) {
+		a11_div=0;
+		writel(hunt_s->a11clk_khz/19200, PLLn_L_VAL(0));
+		cpu_relax();
+		udelay(50);
+	} else if (hunt_s->pll == ACPU_PLL_0) {
+		if ((readl(PLLn_L_VAL(0)) & 0x3f) != PLL_960_MHZ) {
+			/* Restore PLL0 to standard config */
+			writel(PLL_960_MHZ, PLLn_L_VAL(0));
+		}
+		cpu_relax();
+		udelay(50);
+	}
+#endif
 	/* CLK_SEL_SRC1NO */
 	src_sel = reg_clksel & 1;
 
@@ -446,7 +476,11 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 	reg_clkctl = readl(A11S_CLK_CNTL_ADDR);
 	reg_clkctl &= ~(0xFF << (8 * src_sel));
 	reg_clkctl |= hunt_s->a11clk_src_sel << (4 + 8 * src_sel);
+#ifdef CONFIG_MSM7X27_OVERCLOCK
+	reg_clkctl |= a11_div << (0 + 8 * src_sel);
+#else
 	reg_clkctl |= hunt_s->a11clk_src_div << (0 + 8 * src_sel);
+#endif
 	writel(reg_clkctl, A11S_CLK_CNTL_ADDR);
 
 	/* Program clock source selection */
