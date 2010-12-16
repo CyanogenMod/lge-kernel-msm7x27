da0926a7debb9f67f960265a0af72afc18b9c1ac
Stephen Boyd <sboyd@codeaurora.org>
2010-12-15 19:19:19 -0800

    msm: clock: Cleanup whitespace and checkpatch errors
    Miscellaneous cleanup of whitespace errors and also checkpatch
    warnings/errors.
    
    Change-Id: I35d0108f4d602ecfeeb9c68e6dd57bc061dc3ab1
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/clock-local.h
+++ arch/arm/mach-msm/clock-local.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -76,7 +76,7 @@ struct clk_freq_tbl {
 	const uint32_t	cc_val;
 	uint32_t	mnd_en_mask;
 	const unsigned	sys_vdd;
-	void 		*const extra_freq_data;
+	void		*const extra_freq_data;
 };
 
 /* Some clocks have two banks to avoid glitches when switching frequencies.
