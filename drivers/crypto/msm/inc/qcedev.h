515b06698c9f2bfaaeaadeefbc47bac0c7e8a723
Mona Hossain <mhossain@codeaurora.org>
2010-12-23 14:47:29 -0800

    crypto: Add comments and clean-ups
    This patch has the following changes
    - Add MACROS for use_pmem values
    - Rename qcedev_cipher_request to qcedev_cipher_req
    - Add checks for memory failure and some minor clean-ups
    
    Change-Id: I64ba2fd67fa7260e2fb3a2e089e14b563b131562
    Signed-off-by: Mona Hossain <mhossain@codeaurora.org>

--- drivers/crypto/msm/inc/qcedev.h
+++ drivers/crypto/msm/inc/qcedev.h
@@ -40,6 +40,9 @@
 
 #define QCEDEV_MAX_BUFFERS      16
 #define QCEDEV_MAX_SHA_DIGEST	32
+
+#define QCEDEV_USE_PMEM		1
+#define QCEDEV_NO_PMEM		0
 /**
 *qcedev_oper_enum: Operation types
 * @QCEDEV_OPER_ENC:		Encrypt
@@ -149,28 +152,31 @@ struct	qcedev_pmem_info{
 
 /**
 * struct qcedev_cipher_op_req - Holds the ciphering request information
-* @use_pmem:		Flasg to indicate if buffer source is PMEM
-* @pmem:		Stores PMEM buffer information.
+* @use_pmem (IN):	Flag to indicate if buffer source is PMEM
+*			QCEDEV_USE_PMEM/QCEDEV_NO_PMEM
+* @pmem (IN):		Stores PMEM buffer information.
 *			Refer struct qcedev_pmem_info
-* @vbuf:		Stores Source and destination Buffer information
+* @vbuf (IN/OUT):	Stores Source and destination Buffer information
 *			Refer to struct qcedev_vbuf_info
-* @data_len:		Total Length of input/src and output/dst in bytes
-* @in_place_op:		Indicates whether the operation is inplace where
+* @data_len (IN):	Total Length of input/src and output/dst in bytes
+* @in_place_op (IN):	Indicates whether the operation is inplace where
 *			source == destination
 *			When using PMEM allocated memory, must set this to 1
-* @enckey:		128 bits of confidentiality key
+* @enckey (IN):		128 bits of confidentiality key
 *			enckey[0] bit 127-120, enckey[1] bit 119-112,..
 *			enckey[15] bit 7-0
-* @encklen:		Length of the encryption key(set to 128  bits/16
+* @encklen (IN):	Length of the encryption key(set to 128  bits/16
 *			bytes in the driver)
-* @iv:			Initialisation vector data
-* @ivlen:		Length of the IV
-* @byteoffset:		Offset in the Cipher BLOCK (applicable and to be set
+* @iv (IN/OUT):		Initialisation vector data
+*			This is updated by the driver, incremented by
+*			number of blocks encrypted/decrypted.
+* @ivlen (IN):		Length of the IV
+* @byteoffset (IN):	Offset in the Cipher BLOCK (applicable and to be set
 *			for AES-128 CTR mode only)
-* @alg:			Type of ciphering algorithm: AES/DES/3DES
-* @mode:		Mode use when using AES algorithm: ECB/CBC/CTR
+* @alg (IN):		Type of ciphering algorithm: AES/DES/3DES
+* @mode (IN):		Mode use when using AES algorithm: ECB/CBC/CTR
 *			Apllicabel when using AES algorithm only
-* @op:			Type of operation: QCEDEV_OPER_DEC/QCEDEV_OPER_ENC or
+* @op (IN):		Type of operation: QCEDEV_OPER_DEC/QCEDEV_OPER_ENC or
 *			QCEDEV_OPER_ENC_NO_KEY/QCEDEV_OPER_DEC_NO_KEY
 *
 * If use_pmem is set to 0, the driver assumes that  memory was not allocated
@@ -187,7 +193,7 @@ struct	qcedev_pmem_info{
 * by adding the  offsets to the kernel virtual addr.
 */
 struct	qcedev_cipher_op_req {
-	bool				use_pmem;
+	uint8_t				use_pmem;
 	union{
 		struct qcedev_pmem_info	pmem;
 		struct qcedev_vbuf_info	vbuf;
@@ -207,13 +213,13 @@ struct	qcedev_cipher_op_req {
 
 /**
 * struct qcedev_sha_op_req - Holds the hashing request information
-* @data:			Array of pointers to the data to be hashed
-* @entries:			NUmber of buf_info entries in the data array
-* @data_len:			Length of data to be hashed
-* @digest:			Holds the hashed data information
-* @diglen:			Size of the hashed/digest data
-* @alg:				Secure Hash algorithm
-* @ctxt:			RESERVED: User should not modify this data.
+* @data (IN):			Array of pointers to the data to be hashed
+* @entries (IN):		Number of buf_info entries in the data array
+* @data_len (IN):		Length of data to be hashed
+* @digest (IN/OUT):		Returns the hashed data information
+* @diglen (OUT):		Size of the hashed/digest data
+* @alg (IN):			Secure Hash algorithm
+* @ctxt (Reserved):		RESERVED: User should not modify this data.
 */
 struct	qcedev_sha_op_req {
 	struct buf_info			data[QCEDEV_MAX_BUFFERS];
