5ecae33b37a454da4a85d6eac7e227a8f70c7d77
Sujith Reddy Thumma <sthumma@codeaurora.org>
2011-01-25 18:03:52 +0530

    msm: 8x60: Add DFAB clock support for SDC slots
    SDC peripheral clocks are derived from Daytona Fabric core
    clock. Hence, add the required handler for SDCC driver to
    control the clock rate.
    
    Change-Id: I50819a773b35505d13b7f20df3fd59c026dc8cdf
    Signed-off-by: Sujith Reddy Thumma <sthumma@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -6306,6 +6306,7 @@ static struct mmc_platform_data msm8x60_sdc1_data = {
 	.msmsdcc_fmid	= 24000000,
 	.msmsdcc_fmax	= 48000000,
 	.nonremovable	= 1,
+	.pclk_src_dfab	= 1,
 };
 #endif
 
@@ -6318,6 +6319,7 @@ static struct mmc_platform_data msm8x60_sdc2_data = {
 	.msmsdcc_fmid	= 24000000,
 	.msmsdcc_fmax	= 48000000,
 	.nonremovable	= 1,
+	.pclk_src_dfab  = 1,
 	.register_status_notify = sdc2_register_status_notify,
 };
 #endif
@@ -6338,6 +6340,7 @@ static struct mmc_platform_data msm8x60_sdc3_data = {
 	.msmsdcc_fmid	= 24000000,
 	.msmsdcc_fmax	= 48000000,
 	.nonremovable	= 0,
+	.pclk_src_dfab  = 1,
 };
 #endif
 
@@ -6350,6 +6353,7 @@ static struct mmc_platform_data msm8x60_sdc4_data = {
 	.msmsdcc_fmid	= 24000000,
 	.msmsdcc_fmax	= 48000000,
 	.nonremovable	= 1,
+	.pclk_src_dfab  = 1,
 };
 #endif
 
@@ -6362,6 +6366,7 @@ static struct mmc_platform_data msm8x60_sdc5_data = {
 	.msmsdcc_fmid	= 24000000,
 	.msmsdcc_fmax	= 48000000,
 	.nonremovable	= 1,
+	.pclk_src_dfab  = 1,
 	.register_status_notify = sdc5_register_status_notify,
 };
 #endif
