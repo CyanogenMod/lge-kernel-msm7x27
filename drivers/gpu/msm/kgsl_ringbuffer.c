33d9f59e7a1555314f1658fbda9e54482c43bb37
Shubhraprakash Das <sadas@codeaurora.org>
2011-01-27 00:17:10 -0700

    msm_kgsl: Introduce GPU hang state and workqueues for GPU cores
    Introduce a GPU hang state which is used to prevent any commands
    from being submitted to GPU if a hang is detected.
    
    Also, create seperate work queues for GPU cores which allows
    synchronization between scheduled works/tasks and threads
    that execute as part of a process context.
    
    (cherry picked from commit 8e6d444fc716f751ea8832e237e3f46567bfe36b)
    
    Change-Id: I383a9bee55f17cd20445a84f06f857810e7a3bfb
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/kgsl_ringbuffer.c
+++ drivers/gpu/msm/kgsl_ringbuffer.c
@@ -727,6 +727,8 @@ kgsl_ringbuffer_issuecmds(struct kgsl_device *device,
 	KGSL_CMD_VDBG("enter (device->id=%d, flags=%d, cmds=%p, "
 		"sizedwords=%d)\n", device->id, flags, cmds, sizedwords);
 
+	if (device->state & KGSL_STATE_HUNG)
+		return;
 	kgsl_ringbuffer_addcmds(rb, flags, cmds, sizedwords);
 }
 
@@ -749,6 +751,8 @@ kgsl_ringbuffer_issueibcmds(struct kgsl_device_private *dev_priv,
 			device->id, drawctxt_index, (unsigned int)ibdesc,
 			numibs, timestamp);
 
+	if (device->state & KGSL_STATE_HUNG)
+		return -EINVAL;
 	if (!(yamato_device->ringbuffer.flags & KGSL_FLAGS_STARTED) ||
 				(drawctxt_index >= KGSL_CONTEXT_MAX)) {
 		KGSL_CMD_VDBG("return %d\n", -EINVAL);
