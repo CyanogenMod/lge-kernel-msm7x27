1803d4c690874411d1e9a4504d9a0fda621d5ad0
Rajesh Sastrula <vrajesh@codeaurora.org>
2011-01-27 16:43:58 -0800

    msm: board_msm8x60: Fix mdp clock scaling vectors
    modify mdp clock scaling vectors to save power.
    
    (cherry picked from commit 7b48ddf7f008838dbed90f2e1e1aedc3b1ad3ede)
    
    Change-Id: I23b1333e7d15209574a0e1c1c67626b3f60ab297
    Signed-off-by: Rajesh Sastrula <vrajesh@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -7172,13 +7172,13 @@ static int atv_dac_power(int on)
 
 int mdp_core_clk_rate_table[] = {
 	59080000,
-	128000000,
-	160000000,
+	59080000,
+	85330000,
 	200000000,
 };
 static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = MDP_VSYNC_GPIO,
-	.mdp_core_clk_rate = 200000000,
+	.mdp_core_clk_rate = 59080000,
 	.mdp_core_clk_table = mdp_core_clk_rate_table,
 	.num_mdp_clk = ARRAY_SIZE(mdp_core_clk_rate_table),
 #ifdef CONFIG_MSM_BUS_SCALING
