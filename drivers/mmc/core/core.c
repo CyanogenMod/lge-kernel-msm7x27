8a00d603a82395d28a8fdd4aba9d628bad52a2ed
Sahitya Tummala <stummala@codeaurora.org>
2011-02-02 13:39:36 +0530

    mmc: core: Fix mmc_delayed_work wakelock bug in mmc_rescan()
    The mmc_rescan() should extend mmc_delayed_work wakelock only when card
    insertion or removal event is detected. But the current code has a bug
    because of which it always extends this wakelock for every mmc_rescan()
    after card removal. This will be done until the card is detected again.
    This patch fixes this bug by extending the wakelock only when the card
    is removed and not for every mmc_rescan after card removal.
    
    (cherry picked from commit 4e0f9a1d7023f3ccd927b10dbc0d3162c803c0d3)
    
    Change-Id: I2094d34b8d058a84a1034505c8ccf2e8644d9021
    CRs-Fixed: 273265
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1114,14 +1114,15 @@ void mmc_rescan(struct work_struct *work)
 	mmc_bus_get(host);
 
 	/* if there is a card registered, check whether it is still present */
-	if ((host->bus_ops != NULL) && host->bus_ops->detect && !host->bus_dead)
+	if ((host->bus_ops != NULL) && host->bus_ops->detect &&
+		!host->bus_dead) {
 		host->bus_ops->detect(host);
-
-	/* If the card was removed the bus will be marked
-	 * as dead - extend the wakelock so userspace
-	 * can respond */
-	if (host->bus_dead)
-		extend_wakelock = 1;
+		/* If the card was removed the bus will be marked
+		 * as dead - extend the wakelock so userspace
+		 * can respond */
+		if (host->bus_dead)
+			extend_wakelock = 1;
+	}
 
 	mmc_bus_put(host);
 
