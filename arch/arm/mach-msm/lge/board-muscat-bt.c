735dd10744f773932500c07f23792275f9c2859f
kukdong.bae <kukdong.bae@lge.com>
2011-03-08 08:07:47 +0900

    Bluetooth source sync
    Change-Id: I416d14826fae978378657976159a117743deb3ce

--- arch/arm/mach-msm/lge/board-muscat-bt.c
+++ arch/arm/mach-msm/lge/board-muscat-bt.c
@@ -22,6 +22,21 @@
 #include <linux/delay.h>
 #include <linux/rfkill.h>
 
+/* bluetooth gpio pin */
+enum {
+	BT_WAKE         = 42,
+	BT_RFR          = 43,
+	BT_CTS          = 44,
+	BT_RX           = 45,
+	BT_TX           = 46,
+	BT_PCM_DOUT     = 68,
+	BT_PCM_DIN      = 69,
+	BT_PCM_SYNC     = 70,
+	BT_PCM_CLK      = 71,
+	BT_HOST_WAKE    = 83,
+	BT_RESET_N			= 123,
+};
+
 #ifdef CONFIG_BT
 static unsigned bt_config_power_on[] = {
 	GPIO_CFG(BT_WAKE, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA),	/* WAKE */
@@ -36,6 +51,7 @@ static unsigned bt_config_power_on[] = {
 	GPIO_CFG(BT_HOST_WAKE, 0, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA),	/* HOST_WAKE */
 	GPIO_CFG(BT_RESET_N, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA),	/* RESET_N */
 };
+
 static unsigned bt_config_power_off[] = {
 	GPIO_CFG(BT_WAKE, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),	/* WAKE */
 	GPIO_CFG(BT_RFR, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),	/* RFR */
@@ -47,7 +63,7 @@ static unsigned bt_config_power_off[] = {
 	GPIO_CFG(BT_PCM_SYNC, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),	/* PCM_SYNC */
 	GPIO_CFG(BT_PCM_CLK, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),	/* PCM_CLK */
 	GPIO_CFG(BT_HOST_WAKE, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),	/* HOST_WAKE */
-	GPIO_CFG(BT_RESET_N, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),	/* RESET_N */	
+	GPIO_CFG(BT_RESET_N, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),	/* RESET_N */
 };
 
 static int muscat_bluetooth_toggle_radio(void *data, bool state)
@@ -55,7 +71,7 @@ static int muscat_bluetooth_toggle_radio(void *data, bool state)
 	int ret;
 	int (*power_control)(int enable);
 
-    power_control = ((struct bluetooth_platform_data *)data)->bluetooth_power;
+	power_control = ((struct bluetooth_platform_data *)data)->bluetooth_power;
 	ret = (*power_control)((state == RFKILL_USER_STATE_SOFT_BLOCKED) ? 1 : 0);
 	return ret;
 }
@@ -64,7 +80,6 @@ static int muscat_bluetooth_power(int on)
 {
 	int pin, rc;
 
-	
 	printk(KERN_DEBUG "%s\n", __func__);
 	printk( "%s %d\n", __func__, on);
 
@@ -79,28 +94,20 @@ static int muscat_bluetooth_power(int on)
 				return -EIO;
 			}
 		}
-        //Turn Bluetooth Power On if and only if not turned on by WLAN yet.
-//        if (!gpio_get_value(CONFIG_BCM4325_GPIO_WL_REGON)) //#23
-//		    gpio_set_value(CONFIG_BCM4325_GPIO_WL_REGON, 1); //#23
-//		mdelay(100);
 		gpio_set_value(BT_RESET_N, 0);
 		mdelay(100);
 		gpio_set_value(BT_RESET_N, 1);
 		mdelay(100);
 
 	} else {
-        //Turn Bluetooth Power Off if and only if not used by WLAN anymore.
-//        if (!gpio_get_value(CONFIG_BCM4325_GPIO_WL_RESET)) //#93
-//         gpio_set_value(CONFIG_BCM4325_GPIO_WL_REGON, 0); //#23
-
 		gpio_set_value(BT_RESET_N, 0);
 		for (pin = 0; pin < ARRAY_SIZE(bt_config_power_off); pin++) {
 			rc = gpio_tlmm_config(bt_config_power_off[pin],
-					      GPIO_CFG_ENABLE);
+					GPIO_CFG_ENABLE);
 			if (rc) {
 				printk(KERN_ERR
-				       "%s: gpio_tlmm_config(%#x)=%d\n",
-				       __func__, bt_config_power_off[pin], rc);
+						"%s: gpio_tlmm_config(%#x)=%d\n",
+						__func__, bt_config_power_off[pin], rc);
 				return -EIO;
 			}
 		}
@@ -117,19 +124,11 @@ static struct platform_device msm_bt_power_device = {
 	.name = "bt_power",
 	.dev = {
 		.platform_data = &muscat_bluetooth_data,
-	},		
+	},
 };
-
 
 static void __init bt_power_init(void)
 {
-/* LGE_CHANGE_S, [kidong0420.kim@lge.com] , 2010-06-18, for current consumption*/
-  //MI thunderc_bluetooth_power(1);
-//  gpio_set_value(23, 1);
-//  ssleep(1); /* 1 sec */
-  //MI thunderc_bluetooth_power(0);
-//  gpio_set_value(23, 0);
-/* LGE_CHANGE_E, [kidong0420.kim@lge.com] , 2010-06-18, for current consumption*/
 }
 #else
 #define bt_power_init(x) do {} while (0)
@@ -167,7 +166,7 @@ static struct platform_device msm_bluesleep_device = {
 	.resource	= bluesleep_resources,
 	.dev = {
 		.platform_data = &muscat_bluesleep_data,
-	},	
+	},
 };
 
 void __init lge_add_btpower_devices(void)
