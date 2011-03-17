792e772230fdeb2d0f7661e42d63ff9c7c00164a
Sriranjan Srikantam <cssrika@codeaurora.org>
2011-03-17 21:39:59 +0530

    msm: audio: 7x27: Fix suspend issue after audio playback
    Audio decoders doesn't release the resources acquired using RMTASK
    till the decoder instance is closed and RMTASK module is not disabled
    until all the resource are released. DSP votes against TCXO if RMTASK
    is in eanbled state. Hence device doesn't go into TCXO when audio
    playback is in paused state. Fix this issue by releasing the DSP
    resources in decoder disable and acquire the resource back in decoder
    enable so that RMTASK can be disabled in between and device can move
    into TCXO mode in paused state.
    
    CRs-Fixed: 271601 287216
    Signed-off-by: Sriranjan Srikantam <cssrika@codeaurora.org>
    (cherry picked from commit 75ff790bbb93ba2a544a0f63ff37be1cf7987be8)
    
    Change-Id: I3ca84dd20c3092f5ce6c1e5a8c823d2363636a33
    Signed-off-by: Sunil Joseph <sunilj@codeaurora.org>

--- arch/arm/mach-msm/qdsp5/audio_evrc.c
+++ arch/arm/mach-msm/qdsp5/audio_evrc.c
@@ -140,6 +140,7 @@ struct audio {
 	uint8_t buf_refresh:1;
 	int teos; /* valid only if tunnel mode & no data left for decoder */
 	enum msm_aud_decoder_state dec_state;	/* Represents decoder state */
+	int rmt_resource_released;
 
 	const char *module_name;
 	unsigned queue_id;
@@ -178,6 +179,36 @@ static void audevrc_buffer_refresh(struct audio *audio);
 static void audevrc_post_event(struct audio *audio, int type,
 		union msm_audio_event_payload payload);
 
+static int rmt_put_resource(struct audio *audio)
+{
+	struct aud_codec_config_cmd cmd;
+	unsigned short client_idx;
+
+	cmd.cmd_id = RM_CMD_AUD_CODEC_CFG;
+	cmd.client_id = RM_AUD_CLIENT_ID;
+	cmd.task_id = audio->dec_id;
+	cmd.enable = RMT_DISABLE;
+	cmd.dec_type = AUDDEC_DEC_EVRC;
+	client_idx = ((cmd.client_id << 8) | cmd.task_id);
+
+	return put_adsp_resource(client_idx, &cmd, sizeof(cmd));
+}
+
+static int rmt_get_resource(struct audio *audio)
+{
+	struct aud_codec_config_cmd cmd;
+	unsigned short client_idx;
+
+	cmd.cmd_id = RM_CMD_AUD_CODEC_CFG;
+	cmd.client_id = RM_AUD_CLIENT_ID;
+	cmd.task_id = audio->dec_id;
+	cmd.enable = RMT_ENABLE;
+	cmd.dec_type = AUDDEC_DEC_EVRC;
+	client_idx = ((cmd.client_id << 8) | cmd.task_id);
+
+	return get_adsp_resource(client_idx, &cmd, sizeof(cmd));
+}
+
 /* must be called with audio->lock held */
 static int audevrc_enable(struct audio *audio)
 {
@@ -186,6 +217,17 @@ static int audevrc_enable(struct audio *audio)
 
 	if (audio->enabled)
 		return 0;
+
+	if (audio->rmt_resource_released == 1) {
+		audio->rmt_resource_released = 0;
+		rc = rmt_get_resource(audio);
+		if (rc) {
+			MM_ERR("ADSP resources are not available for EVRC \
+				session 0x%08x on decoder: %d\n Ignoring \
+				error and going ahead with the playback\n",
+				(int)audio, audio->dec_id);
+		}
+	}
 
 	audio->dec_state = MSM_AUD_DECODER_STATE_NONE;
 	audio->out_tail = 0;
@@ -245,6 +287,8 @@ static int audevrc_disable(struct audio *audio)
 		if (audio->pcm_feedback == TUNNEL_MODE_PLAYBACK)
 			audmgr_disable(&audio->audmgr);
 		audio->out_needed = 0;
+		rmt_put_resource(audio);
+		audio->rmt_resource_released = 1;
 	}
 	return rc;
 }
@@ -506,36 +550,6 @@ static void audevrc_config_hostpcm(struct audio *audio)
 	cfg_cmd.partition_number = 0;
 	audplay_send_queue0(audio, &cfg_cmd, sizeof(cfg_cmd));
 
-}
-
-static int rmt_put_resource(struct audio *audio)
-{
-	struct aud_codec_config_cmd cmd;
-	unsigned short client_idx;
-
-	cmd.cmd_id = RM_CMD_AUD_CODEC_CFG;
-	cmd.client_id = RM_AUD_CLIENT_ID;
-	cmd.task_id = audio->dec_id;
-	cmd.enable = RMT_DISABLE;
-	cmd.dec_type = AUDDEC_DEC_EVRC;
-	client_idx = ((cmd.client_id << 8) | cmd.task_id);
-
-	return put_adsp_resource(client_idx, &cmd, sizeof(cmd));
-}
-
-static int rmt_get_resource(struct audio *audio)
-{
-	struct aud_codec_config_cmd cmd;
-	unsigned short client_idx;
-
-	cmd.cmd_id = RM_CMD_AUD_CODEC_CFG;
-	cmd.client_id = RM_AUD_CLIENT_ID;
-	cmd.task_id = audio->dec_id;
-	cmd.enable = RMT_ENABLE;
-	cmd.dec_type = AUDDEC_DEC_EVRC;
-	client_idx = ((cmd.client_id << 8) | cmd.task_id);
-
-	return get_adsp_resource(client_idx, &cmd, sizeof(cmd));
 }
 
 static void audevrc_send_data(struct audio *audio, unsigned needed)
@@ -1240,7 +1254,8 @@ static int audevrc_release(struct inode *inode, struct file *file)
 	MM_INFO("audio instance 0x%08x freeing\n", (int)audio);
 	mutex_lock(&audio->lock);
 	audevrc_disable(audio);
-	rmt_put_resource(audio);
+	if (audio->rmt_resource_released == 0)
+		rmt_put_resource(audio);
 	audevrc_flush(audio);
 	audevrc_flush_pcm_buf(audio);
 	msm_adsp_put(audio->audplay);
