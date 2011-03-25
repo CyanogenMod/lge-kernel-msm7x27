55c267481e314eea21835954406f16cb06799cae
Munyoung Hwang <munyoung.hwang@lge.com>
2011-03-25 16:37:49 +0900

    [P690]LCD: apply changing LUT for improving display quality
    Change-Id: Ia1d7c9a391bb189c94f691866eae709d3ec644a4

--- drivers/video/msm/mdp_hw.h
+++ drivers/video/msm/mdp_hw.h
@@ -766,5 +766,8 @@ void mdp_ppp_dump_debug(const struct mdp_info *mdp);
 #define MDDI_VDO_PACKET_PRIM  0xC3
 #define MDDI_VDO_PACKET_SECD  0xC0
 
+#if defined(CONFIG_MACH_MSM7X27_THUNDERG) || defined(CONFIG_MACH_MSM7X27_GELATO)
+extern mdp_load_thunder_lut(int lut_type);
+#endif
 
 #endif
