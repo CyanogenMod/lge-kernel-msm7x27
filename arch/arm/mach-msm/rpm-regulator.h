c6dc35acdb8af41ebb3e0cfadacfff1225277756
David Collins <collinsd@codeaurora.org>
2011-04-15 16:12:21 -0700

    msm: rpm-regulator: Add API for runtime switching freq modification
    Some consumers require the ability to modify the switching frequency
    of SMPS regulators at run time.  Add an rpm-regulator API which
    provides a means to change this switching frequency.
    
    (cherry picked from commit f03fcb65a8175f69ddbaa73ab6b9e568d5265d69)
    
    Change-Id: Ie1c0af5269a6a70c1a6e3166f0f6966b58e75b93
    CRs-Fixed: 283902
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- arch/arm/mach-msm/rpm-regulator.h
+++ arch/arm/mach-msm/rpm-regulator.h
@@ -190,7 +190,36 @@ enum rpm_vreg_voter {
 	RPM_VREG_VOTER_COUNT,
 };
 
+/**
+ * rpm_vreg_set_voltage - vote for a min_uV value of specified regualtor
+ * @vreg: ID for regulator
+ * @voter: ID for the voter
+ * @min_uV: minimum acceptable voltage (in uV) that is voted for
+ * @sleep_also: 0 for active set only, non-0 for active set and sleep set
+ *
+ * Returns 0 on success or errno.
+ *
+ * This function is used to vote for the voltage of a regulator without
+ * using the regulator framework.  It is needed by consumers which hold spin
+ * locks or have interrupts disabled because the regulator framework can sleep.
+ * It is also needed by consumers which wish to only vote for active set
+ * regulator voltage.
+ *
+ * If sleep_also == 0, then a sleep-set value of 0V will be voted for.
+ *
+ * This function may only be called for regulators which have the sleep flag
+ * specified in their private data.
+ */
 int rpm_vreg_set_voltage(enum rpm_vreg_id vreg_id, enum rpm_vreg_voter voter,
 			 int min_uV, int sleep_also);
+
+/**
+ * rpm_vreg_set_frequency - sets the frequency of a switching regulator
+ * @vreg: ID for regulator
+ * @min_uV: minimum acceptable frequency of operation
+ *
+ * Returns 0 on success or errno.
+ */
+int rpm_vreg_set_frequency(enum rpm_vreg_id vreg_id, enum rpm_vreg_freq freq);
 
 #endif
